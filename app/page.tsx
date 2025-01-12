e61fa9b3007ec9156e2f3424986c3a3988aea270
Rahul-vijaykumar-kulkarni <r40kulkarni@gmail.com>
2025-01-12 10:26:09 +0530

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,8 @@ import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
-// import Experience from "@/components/Experience";
+// 
+
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
